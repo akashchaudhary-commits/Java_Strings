217d326ee49075b0905e1f9670d5c8725ccecf08
chAkash3101 <akash.ch3109@gmail.com>
2026-08-23 13:52:47 +0530

    2 commit

---
+++ StringClass/src/ArraysToString.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+public class ArraysToString {
+    public static void main(String[] args)
+    {
+        // Convert char[] to strings
+        char[] c = {'q','w','e','r','t','y'};
+        String str1 = new String(c);
+        // or
+        String st = String.valueOf(c);
+        // only works with char[]
+        // works with just single values
+        System.out.println(str1);
+        System.out.println(st);
+        System.out.println(Arrays.toString(c));
+
+        // int[] to strings
+        int[] arr = {1,2,3,4,5};
+        String str2 = Arrays.toString(arr);
+        System.out.println(str2);
+        //
+        String str3 = String.valueOf(arr);
+        System.out.println(str3);
+
+        // String[] to String
+        String[] s = {"AKash", "bcuvv", "ubqc"};
+        System.out.println(Arrays.toString(s));
+
+        //
+
+
+    }
+}
